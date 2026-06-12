9d4022671193b5b5ffc234b197b48e849f316d43
Bavya <bavya.raghu11@gmail.com>
2026-06-12 14:16:37 +0530

    Connected with OAuth

--- backend/auth-platform/src/main/java/com/bavya/authservice/config/SecurityConfig.java
+++ backend/auth-platform/src/main/java/com/bavya/authservice/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.bavya.authservice.jwt.JwtAuthenticationFilter;
 import com.bavya.authservice.ratelimit.RateLimitFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -35,11 +36,18 @@ public class SecurityConfig {
                                 "/api/auth/refresh",
                                 "/api/auth/logout",
                                 "/api/health",
-                                "/api/client/**"
+                                "/api/client/**",
+                                "/oauth2/**",
+                                "/login/**"
                         )
                         .permitAll()
                         .anyRequest()
                         .authenticated()
+                ).oauth2Login(oauth -> oauth
+                        .defaultSuccessUrl(
+                                "/api/health",
+                                true
+                        )
                 );
 
         http.addFilterBefore(
